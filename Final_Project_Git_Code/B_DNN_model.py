d2d99f9d89fe7380581b42d69793ff02eb609c53
zuyeyang <zuyeyang@gmail.com>
2021-11-22 17:41:49 -0800

    Update B_DNN_model.py

--- Final_Project_Git_Code/B_DNN_model.py
+++ Final_Project_Git_Code/B_DNN_model.py
@@ -16,27 +16,40 @@ def recover(x,x_train):
 
 def data_split(X,y_with_key):
     i = y_with_key.shape[1]
-    print(i)
     X_copy = X.copy()
     y_with_key_copy = y_with_key.copy()
-    X_train, X_test, y_train_coef, y_test_coef = train_test_split(X_copy, y_with_key_copy, test_size=0.2, random_state=0)
+    X_train, X_test, y_train_coef, y_test_coef = train_test_split(X_copy, y_with_key_copy, test_size=0.13, random_state=42)
+    X_train, X_val, y_train_coef, y_val_coef = train_test_split(X_copy, y_with_key_copy, test_size=0.13, random_state=42)
     y_train = y_train_coef[:,0:i-1]
+    y_val = y_val_coef[:,0:i-1]
     y_test = y_test_coef[:,0:i-1]
 
     y_train_norm = norm(y_train,y_train)
+    y_val_norm =  norm(y_val,y_train)
     y_test_norm =  norm(y_test,y_train)
-    return X_train,X_test,y_train_coef,y_test_coef,y_train,y_test,y_train_norm,y_test_norm
+    
+    return X_train,X_test,X_val, y_train_coef,y_test_coef,y_val_coef,y_train,y_test,y_val,y_train_norm,y_test_norm,y_val_norm
 
-def sequential_model(l1,l2,loss_method,X_train,y_train):
+def sequential_model(loss_method,X_train,y_train,nl,opt):
     in_dim= X_train.shape[1]
     out_dim= y_train.shape[1]
-    model0 = Sequential([
-    Dense(l1, input_dim=in_dim, activation="relu"),
-    Dense(l2, activation="relu"),
-    Dense(out_dim)
-    ])
+    model0 = Sequential()
+    model0.add(Dense(nl[0], input_dim=in_dim, activation='relu'))
+    if len(nl) == 2:
+        model0.add(Dense(nl[1], activation='relu'))
+    if len(nl) == 3:
+        model0.add(Dense(nl[2], activation='relu'))
+    if len(nl) == 4:
+        model0.add(Dense(nl[3], activation='relu'))
+    model0.add(Dense(out_dim))
+    #model0 = Sequential([
+    #Dense(l1, input_dim=in_dim, activation="relu"),
+    #Dense(l2, activation="relu"),
+    #Dense(l3, activation="relu"),
+    #Dense(out_dim)
+    #])
     model0.compile(loss=loss_method,
-                    optimizer="adam",
+                    optimizer=opt,
                     metrics = ['accuracy'])
     return model0
 
@@ -70,3 +83,22 @@ def pred_error(y,key,all_metrics_df,objective):
         rms[i] = np.sqrt(np.sum(np.square((acapacity_test - capacity_exp))) / len(capacity_exp)) * len(capacity_exp) / np.sum(capacity_exp)
         mae[i] = np.sum(np.abs(acapacity_test - capacity_exp)) / len(capacity_exp) 
         return rms, mae
+
+def error_after_plot(y,yt,key,all_metrics_df,objective):
+    itera,num_var = y.shape
+    num_var = num_var-1
+    difference = []
+    for i in key:
+        cell_num = i
+        for_one_cell = all_metrics_df.loc[(all_metrics_df.seq_num == cell_num)]
+        #extract the cycle number
+        x_range = np.floor(for_one_cell['equivalent_full_cycles'].max())
+        x = np.arange(x_range)
+        selected = y[:,-1] == cell_num
+        if num_var == 3:
+            a,b,c = y[selected,0:num_var][0]
+            at,bt,ct = yt[selected,0:num_var][0]
+            acapacity_test_1 = objective(x,a,b,c)
+            acapacity_test_t = objective(x,at,bt,ct)
+        difference.append(np.sum(acapacity_test_1-acapacity_test_t))
+    return np.mean(np.abs(difference))
